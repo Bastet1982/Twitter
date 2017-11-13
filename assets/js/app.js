221a1f72012f5bd6b869b7e58aef9b1fd7a5c2a5
Bastet1982 <oriele.nieto@gmail.com>
2017-11-13 00:17:39 -0300

    twitter avanzado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,45 +1,50 @@
 
-//Twitter primera parte,debo generar un posteo en pantalla al comentario de mi text area.
-//Debo permitir que el button al hacer click, logre el posteo.
+//Generar contador
+     
+//1- primera parte,debo generar un posteo en pantalla al comentario de mi text area.
+//2-Debo permitir que el button al hacer click, logre el posteo.
+     
+function add(){  //creo una función que va a agregar el comentario y la mostrara una vez presionado el boton tweet
 
-
-function add(){  //Se almacena el valor//
     var comments = document.getElementById('comment').value;  //Rescato el elemento ID con .value//
      document.getElementById('comment').value =""; //Cuando agarre el valor este sera limpiado del text área//
-
      var newComments= document.createElement('div'); //div con todo lo que vendrá
+     
      var cont= document.getElementById('cont'); //agrega un id al contenedor div que se creo arriba//
 
-     var check= document.createElement('input');
+     var check= document.createElement('input'); //Al momento de darle clik al boton, se genera un check que seria el tweet ya posteado
      check.type= 'checkbox';
 
      //ORDEN//
 
      var paragraph= document.createElement('p'); //Debe ponerse esta variable para que me permita incluir el texto (p)//
-     paragraph.classList.add("color"); //es solo el elemento pe, pero no hay nada dentro del p//
+     paragraph.classList.add("color"); //es solo el elemento pe, pero no hay nada dentro del p hasta que se haga click//
 
 
      var nodoText= document.createTextNode(comments);  //se le llama sin comillas por ser una variable, y corresponde a la primera variable//
-     paragraph.appendChild(nodoText); //nodotext hijo del padre//
+     paragraph.appendChild(nodoText); //nodotext hijo del padre paragraph//
 
     //Es englobado todo desde lo micro a lo macro, menos a mas//
-     newComments.appendChild(check); 
-     newComments.appendChild(paragraph);
-     cont.appendChild(newComments); //hijo del cont//
 
-
+    paragraph.textContent = comment; //hijo de la variable cont. realizada al inicio
+    newComments.insertBefore()
    }    
 
-// Debo impedir que el button funcione si hay texto vacio//
-     
-function desactiva(){
-	if ('comment' == ""){ //si el comentario es = vacio, el boton se desactiva
-	    button= disabled;
-	}else('comment > 0'){ // si el comentario es  mayor a 0 el boton funciona
-		button =enabled;
-	}
-}
 
-// Conteo regresivo tweeter//
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
  //Ingresar hora del posteo//
